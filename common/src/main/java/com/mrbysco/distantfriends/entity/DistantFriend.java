d77fdd0673fd8a36eb4095a06e46d202ea634105
Mrbysco <Mrbysco@users.noreply.github.com>
2025-09-21 20:05:08 +0200

    Update swing time properly (for arm swinging)
    Make Friend renderer extend HumanoidMobRenderer

--- common/src/main/java/com/mrbysco/distantfriends/entity/DistantFriend.java
+++ common/src/main/java/com/mrbysco/distantfriends/entity/DistantFriend.java
@@ -137,6 +137,12 @@ public class DistantFriend extends PathfinderMob {
 	}
 
 	@Override
+	public void aiStep() {
+		this.updateSwingTime();
+		super.aiStep();
+	}
+
+	@Override
 	protected void customServerAiStep(ServerLevel level) {
 		if (this.tickCount > 80 && tickCount % 20 == 0 && this.isAlive()) {
 			if (!level.getNearbyPlayers(findPlayerCondition, this, this.getBoundingBox().inflate(16.0D, 32.0D, 16.0D)).isEmpty()) {
@@ -163,8 +169,10 @@ public class DistantFriend extends PathfinderMob {
 	@Override
 	public void load(CompoundTag tag) {
 		super.load(tag);
-		setInView(tag.getBooleanOr("inView", false));
-		setLookedAt(tag.getBooleanOr("lookedAt", false));
+		if (tag.contains("inView"))
+			setInView(tag.getBooleanOr("inView", false));
+		if (tag.contains("lookedAt"))
+			setLookedAt(tag.getBooleanOr("lookedAt", false));
 		boolean profileExists = tag.getBooleanOr("profileExists", false);
 		if (profileExists) {
 			entityData.set(RESOLVABLE_PROFILE, ResolvableProfile.CODEC
@@ -204,7 +212,6 @@ public class DistantFriend extends PathfinderMob {
 
 		return spawnDataIn;
 	}
-
 
 	public boolean isLookingAtMe(Player player) {
 		Vec3 vec3 = player.getViewVector(1.0F).normalize();
